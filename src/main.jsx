8cdecc0ab43bda8104cf1bfbb1523b2a0468bc3d
Paulo R Cavalcanti <roma@dcc.ufrj.br>
2024-10-24 06:12:29 -0300

    Missing createRoot doc.

--- src/main.jsx
+++ src/main.jsx
@@ -17,6 +17,18 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+/**
+ * The react-dom/client APIs let you render React components on the client (in the browser).
+ * @external react-dom/client
+ * @see {@link https://react.dev/reference/react-dom/client React DOM APIs}
+ */
+
+/**
+ * Lets you create a root to display React components inside a browser DOM node.
+ * @function createRoot
+ * @memberof external:react-dom/client
+ * @see {@link https://react.dev/reference/react-dom/client/createRoot createRoot}
+ */
 createRoot(document.getElementById("root")).render(
     <StrictMode>
         <App />
